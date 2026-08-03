9d66330353eac39582b6e694ecd8219a35248e2b
Chandru M <sherlockholmes42007@gmail.com>
2026-08-03 14:57:43 +0530

    feat(connectors): add production connector for service_094 (#93)

---
+++ packages/connector_service_094/service_094/__init__.py
@@ -0,0 +1,1 @@
+from .connector import Service094Connector
---
+++ packages/connector_service_094/tests/test_connector.py
@@ -0,0 +1,16 @@
+"""
+Unit tests for Service094 Connector.
+"""
+
+import pytest
+from service_094.connector import Service094Connector
+
+def test_connection_success():
+    connector = Service094Connector(api_key="test_key")
+    assert connector.connect() is True
+
+def test_query_execution():
+    connector = Service094Connector(api_key="test_key")
+    connector.connect()
+    res = connector.execute_query("SELECT 1")
+    assert res["status"] == "success"
